7b1acbd22ce534bfa480ae2f3938fc955a9590a2
Shashi Ross <43995788+Sross123@users.noreply.github.com>
2023-08-14 09:57:55 +0530

    Second COmmit

---
+++ navigation/navigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../screen/Home";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={Home}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
